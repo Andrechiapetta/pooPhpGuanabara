d497ed1cc00c94b1adda4be32e9ef1330ae13596
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-07-25 13:26:04 -0300

    Aula 03

--- Aula03/index.php
+++ Aula03/index.php
@@ -5,10 +5,16 @@
     <title>Aula 03 - POO</title>
 </head>
 <body>
+    <pre>
     <?php
         require_once 'Caneta.php';
-        $c1 = new Caneta;      
+        $c1 = new Caneta;
+        $c1->cor = "Azul";
+        $c1->modelo ="BIC cristal";
+        print_r($c1);
+        $c1->rabiscar();      
     ?>
+    </pre>
     
 </body>
 </html>

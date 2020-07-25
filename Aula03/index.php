f5dd2234e18f885c381a72677c3fa4f5aa885128
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-07-25 13:16:12 -0300

    Aula 03

---
+++ Aula03/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 03 - POO</title>
+</head>
+<body>
+    <?php
+        require_once 'Caneta.php';
+        $c1 = new Caneta;      
+    ?>
+    
+</body>
+</html>
